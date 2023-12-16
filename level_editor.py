5eea7b897767736574ed037f833697801efae938
Fonetus <146757728+Fonetus@users.noreply.github.com>
2023-12-16 15:10:43 +0300

    nazvania

--- level_editor.py
+++ level_editor.py
@@ -90,11 +90,11 @@ for row in range(20):
 	world_data.append(r)
 
 '''
-the boundary of the game world is defined by setting specific tiles to different values as follows:
+The boundary of the game world is defined by setting specific tiles to different values as follows:
 :the bottom row (row 19) is set to 2.
 :the top row (row 0) is set to 1.
-- The leftmost column (column 0) is set to 1.
-- The rightmost column (column 19) is set to 1.
+:the leftmost column (column 0) is set to 1.
+:the rightmost column (column 19) is set to 1.
 '''
 for tile in range(0, 20):
 	world_data[19][tile] = 2
@@ -102,20 +102,41 @@ for tile in range(0, 20):
 	world_data[tile][0] = 1
 	world_data[tile][19] = 1
 
-#function for outputting text onto the screen
+'''
+A function named draw_text is defined for outputting text onto the screen with the following parameters:
+:text: Text to be displayed
+:font: Font type and size
+:text_col: Colour of the text
+:params x, y: Coordinates for positioning the text.
+'''
 def draw_text(text, font, text_col, x, y):
 	img = font.render(text, True, text_col)
 	screen.blit(img, (x, y))
 
+'''
+The draw_grid function is defined to draw grid lines on the game window:
+:vertical Lines: Drawn at intervals of tile_size from the left side to the right side of the screen.
+:horizontal Lines: Drawn at intervals of tile_size from the top to the bottom of the screen, excluding the margin.
+'''
 def draw_grid():
 	for c in range(21):
-		#vertical lines
 		pygame.draw.line(screen, white, (c * tile_size, 0), (c * tile_size, screen_height - margin))
-		#horizontal lines
 		pygame.draw.line(screen, white, (0, c * tile_size), (screen_width, c * tile_size))
 
-
+'''
+The draw_world function displays the game world on the screen. It uses data from world_data to determine which blocks to display and in which location.
+'''
 def draw_world():
+	'''
+ 	The work process:
+ 	:for each row in the range from 0 to 19:
+	:for each column in the range from 0 to 19:
+ 	:if the value in world_data for this row and column is greater than 0:
+  	:if the value is 1 - create a dirt_img image in size (tile_size, tile_size), display the image on the screen in position (col * tile_size, row * tile_size)
+   	:if the value is 2 - create a grass_img image in size (tile_size, tile_size), display the image on the screen in position (col * tile_size, row * tile_size)
+	:if the value is 6 - create a lava_img image with the size (tile_size, tile_size // 2), display the image on the screen at the position (col * tile_size, row * tile_size + (tile_size // 2))
+ 	:if the value is 8 - create an exit_img image with the size (tile_size, int(tile_size * 1.5)), display the image on the screen at the position (col * tile_size, row * tile_size - (tile_size // 2))
+ 	'''
 	for row in range(20):
 		for col in range(20):
 			if world_data[row][col] > 0:
@@ -138,21 +159,48 @@ def draw_world():
 					img = pygame.transform.scale(exit_img, (tile_size, int(tile_size * 1.5)))
 					screen.blit(img, (col * tile_size, row * tile_size - (tile_size // 2)))
 				
-
+'''
+py:class:: Button
+Represents a button with a specific image and behavior.
+'''
 class Button():
+	'''
+ 	:py:method:: __init__(x, y, image)
+
+        Initialize the Button.
+
+        :param x: The x-coordinate of the button
+        :type x: int
+        :param y: The y-coordinate of the button
+        :type y: int
+        :param image: The image of the button
+        :type image: pygame.Surface
+ 	'''
 	def __init__(self, x, y, image):
 		self.image = image
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
 		self.clicked = False
 
+	'''
+ 	:py:method:: draw()
+
+        Draw the button and handle the user's actions.
+
+        :return: action - Indicates if the button has been clicked
+        :rtype: boolean
+ 	'''
 	def draw(self):
 		action = False
 
-		#get mouse position
+		'''
+  		Mouse's position
+  		'''
 		pos = pygame.mouse.get_pos()
 
-		#check mouseover and clicked conditions
+		'''
+		check mouseover and clicked conditions
+  		'''
 		if self.rect.collidepoint(pos):
 			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
 				action = True
@@ -161,62 +209,95 @@ class Button():
 		if pygame.mouse.get_pressed()[0] == 0:
 			self.clicked = False
 
-		#draw button
+		'''
+ 		draw button
+  		'''
 		screen.blit(self.image, (self.rect.x, self.rect.y))
 
 		return action
 
-#create load and save buttons
+'''
+create and save buttons
+'''
 save_button = Button(screen_width // 2 - 150, screen_height - 80, save_img)
 load_button = Button(screen_width // 2 + 50, screen_height - 80, load_img)
 
-#main game loop
+'''
+Main game loop
+'''
 run = True
 while run:
 
 	clock.tick(fps)
 
-	#draw background
+	'''
+ 	:background
+ 	'''
 	screen.fill(green)
 	screen.blit(bg_img, (0, 0))
 
-
-	#load and save level
+	'''
+	load and save level
+	'''
 	if save_button.draw():
-		#save level data
+		
+		'''
+  		save level data
+  		'''
 		pickle_out = open(f'level{level}_data', 'wb')
 		pickle.dump(world_data, pickle_out)
 		pickle_out.close()
+		
 	if load_button.draw():
-		#load in level data
+		
+		'''
+		load in level data
+		'''
 		if path.exists(f'level{level}_data'):
 			pickle_in = open(f'level{level}_data', 'rb')
 			world_data = pickle.load(pickle_in)
 
-
-	#show the grid and draw the level tiles
+	'''
+	show the grid and draw the level tiles
+	'''
 	draw_grid()
 	draw_world()
 
 
-	#text showing current level
+	'''
+	text showing current level
+	'''
 	draw_text(f'Level: {level}', font, white, tile_size, screen_height - 60)
 	draw_text('Press UP or DOWN to change level', font, white, tile_size, screen_height - 40)
 
-	#event handler
+	'''
+	event handler
+	'''
 	for event in pygame.event.get():
-		#quit game
+		
+		'''
+  		quit game
+  		'''
 		if event.type == pygame.QUIT:
 			run = False
-		#mouseclicks to change tiles
+			
+		'''
+  		mouseclicks to change tiles
+  		'''
 		if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
 			clicked = True
 			pos = pygame.mouse.get_pos()
 			x = pos[0] // tile_size
 			y = pos[1] // tile_size
-			#check that the coordinates are within the tile area
+
+			'''
+   			check that the coordinates are within the tile area
+   			'''
 			if x < 20 and y < 20:
-				#update tile value
+
+				'''
+				update tile value
+				'''
 				if pygame.mouse.get_pressed()[0] == 1:
 					world_data[y][x] += 1
 					if world_data[y][x] > 8:
@@ -227,14 +308,18 @@ while run:
 						world_data[y][x] = 8
 		if event.type == pygame.MOUSEBUTTONUP:
 			clicked = False
-		#up and down key presses to change level number
+		'''
+  		up and down key presses to change level number
+  		'''
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
 				level += 1
 			elif event.key == pygame.K_DOWN and level > 1:
 				level -= 1
 
-	#update game display window
+	'''
+ 	update game display window
+ 	'''
 	pygame.display.update()
 
 pygame.quit()
